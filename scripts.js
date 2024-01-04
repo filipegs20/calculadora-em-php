5a670c59ab75c827baed659a53d085babf573c8c
filipegs20 <121211890+filipegs20@users.noreply.github.com>
2024-01-03 21:21:24 -0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ $('#login-form').submit(function(e){
     var s_distancia = $('#distancia').val();
     var s_combustivel = $('#combustivel').val();
     var s_consumo = $('#consumo').val();
-
+    
     $.ajax({
-        url:'http://localhost/calculadora-em-php/calculadora-em-php.php',
+        url:'http://localhost/calculadora-em-php/processamento-de-calculo.php',
         method: 'POST',
         data: { distancia: s_distancia, combustivel: s_combustivel, consumo: s_consumo },
         dataType: 'json',
@@ -14,11 +14,11 @@ $('#login-form').submit(function(e){
     }).done(function(result) {
         // Verifica se há um resultado válido antes de exibir
         if (result.resultado !== undefined) {
-            // Exibe o resultado diretamente na página
-            $('#div-result').html("<div class='form-group'><h3>" + result.resultado + "</h3></div>");
+            // Exibicao do resultado diretamente na pagina
+            $('#div-result').html("<div class='form-group'><h3>O custo com combustível será de R$" + result.resultado + ".</h3></div>");
         } else {
-            // Se houver um erro ou algo inesperado, exiba uma mensagem de erro
-            $('#div-result').html("<div class='form-group'><h3>Erro ao calcular</h3></div>");
+            // Se houver um erro ou algo inesperado, exibe uma mensagem de erro
+            $('#div-result').html("<div class='form-group'><h4>Erro ao calcular</h4></div>");
         }
     }).fail(function(jqXHR, textStatus, errorThrown) {
         console.error('Erro na requisição:', textStatus, errorThrown);
